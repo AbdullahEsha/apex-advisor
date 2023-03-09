9d072717ba3162c632845bc9e8a804ec49039246
AbdullahIsha <shahariaresha@gmail.com>
2023-03-09 18:17:04 +0600

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
-import '@/styles/globals.css'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect } from 'react'
+import "@/styles/globals.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    AOS.init()
-  }, [])
-  return <Component {...pageProps} />
+    AOS.init();
+  }, []);
+  return <Component {...pageProps} />;
 }
